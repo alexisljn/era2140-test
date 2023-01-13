99e57d2dfbe524841b790863f3bc5c0af4a8b7fb
alexisljn <alexis.lejeune@gmx.fr>
2023-01-13 21:36:01 +0100

    create AppContext

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, {createContext, useState} from 'react';
+import {providers} from "ethers";
+
+interface AppContextInterface {
+    provider: providers.Web3Provider | undefined | null;
+}
+
+const AppContext = createContext<AppContextInterface>({
+    provider: undefined
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [provider, setProvider] = useState<providers.Web3Provider | undefined | null>(undefined);
+
+    return (
+        <AppContext.Provider value={{provider}}>
+            <div className="App">
+                <header className="App-header">
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        </AppContext.Provider>
+    );
 }
 
 export default App;
