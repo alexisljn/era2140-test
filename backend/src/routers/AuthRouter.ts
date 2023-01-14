1c79daeceb555a89d7806468520884f6c777c5e7
alexisljn <alexis.lejeune@gmx.fr>
2023-01-14 17:20:21 +0100

    create logic for POST /auth

--- backend/src/routers/AuthRouter.ts
+++ backend/src/routers/AuthRouter.ts
@@ -1,8 +1,58 @@
-import {Router} from "express";
+import {NextFunction, Request, Response, Router} from "express";
+import {formatAddressWithChecksum} from "../utils/Utils";
+import {ErrorData} from "../types/CommonTypes";
+import {generateMessageToSign, checkSignedMessage, generateAccessToken} from "../utils/AuthUtils";
 
 const authRouter: Router = Router();
 
-authRouter.get('/', (req, res, next) => {
+authRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // Missing parameters
+        if (!req.body.hasOwnProperty('address') || !req.body.hasOwnProperty('message')) {
+            const errorData: ErrorData = {status: 422, message: 'Missing parameter address or message'};
+
+            throw new Error(JSON.stringify(errorData));
+        }
+
+        const {address, message} = req.body;
+
+        // Invalid type
+        if (typeof address !== "string" || typeof message !== "string") {
+            const errorData: ErrorData = {status: 422, message: 'Invalid type for parameter address or message'};
+
+            throw new Error(JSON.stringify(errorData));
+        }
+
+        formatAddressWithChecksum(address);
+
+        const signedMessageCheck = checkSignedMessage(address, message);
+
+        // Invalid signature
+        if (!signedMessageCheck) {
+            const errorData: ErrorData = {status: 401, message: 'Address don\'t match with signed message'};
+
+            throw new Error(JSON.stringify(errorData));
+        }
+
+        const accessToken = generateAccessToken(address);
+
+        res.status(200).json({accessToken});
+    } catch (e) {
+        if (e.message.includes('invalid address')) {
+            const errorData: ErrorData = {status: 422, message: `Invalid address: ${req.body.address}`}
+
+            throw new Error(JSON.stringify(errorData));
+        }
+
+        if (e.message.includes("signature")) {
+            const errorData: ErrorData = {status: 422, message: 'Invalid signature'}
+
+            throw new Error(JSON.stringify(errorData));
+        }
+
+        next(e);
+    }
+});
 
 });
 
