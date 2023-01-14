105667f15e27e4325afd66c504d89eed73437817
alexisljn <alexis.lejeune@gmx.fr>
2023-01-14 13:23:23 +0100

    create middleware to handle CORS preflight request

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,18 @@
-import express from 'express';
+import express, {NextFunction, Request, Response} from 'express';
+import {authRouter} from "./routers/AuthRouter";
+import {ErrorData} from "./types/CommonTypes";
 
 const app = express();
 const port = 3000
 
+// CORS Preflight request
+app.options('*', (req: Request, res: Response) => {
+    res.set('Access-Control-Allow-Origin', String(process.env.NODE_ENV) === 'development' ? '*' : String(process.env.FRONTEND_URL));
+    res.set('Access-Control-Allow-Methods', 'GET, POST');
+    res.set('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+
+    res.status(200).send();
+});
 app.get('/', (req, res) => {
     res.send('Hello World!!')
 })
