1a1ddec5eebba29b63ae7a0f5f4b32b1872cf3d6
alexisljn <alexis.lejeune@gmx.fr>
2023-01-14 10:39:44 +0100

    Connect wallet components logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,40 @@
-import React, {createContext, useEffect, useState} from 'react';
+import React, {createContext, useCallback, useEffect, useState} from 'react';
 import {providers} from "ethers";
+import {connectWallet} from "./utils/ProviderUtils";
 
 interface AppContextInterface {
     provider: providers.Web3Provider | undefined | null;
+    address: string | null;
+    changeAddress: (address: string | null) => void;
 }
 
 const AppContext = createContext<AppContextInterface>({
-    provider: undefined
+    provider: undefined,
+    address: null,
+    changeAddress: () => {},
 });
 
 function App() {
+
     const [provider, setProvider] = useState<providers.Web3Provider | undefined | null>(undefined);
+
+    const [address, setAddress] = useState<string | null>(null);
+
+    const changeAddress = useCallback((address: string | null) => {
+        setAddress(address);
+    }, []);
+
+    //TODO Deport
+    const onConnectWallet = useCallback(async () => {
+        try {
+            const address = await connectWallet(provider!);
+
+            changeAddress(address);
+        } catch (e: any) {
+            console.error(e) // Logging for user
+        }
+
+    }, [provider])
 
     useEffect(() => {
         if (window.ethereum) {
@@ -20,26 +44,25 @@ function App() {
         }
     }, []);
 
-    console.log("provider", provider);
+    useEffect(() => {
+        if (!provider) return;
 
-    //TODO if provider is null or undefine
+        (async () => {
+            setChainId((await provider.getNetwork()).chainId);
+        })();
+
+    }, [provider])
+
+    // console.log("provider", provider);
+    // console.log("chainId", chainId);
+    console.log('address', address);
+
+    //TODO if provider is null or undefined
 
     return (
-        <AppContext.Provider value={{provider}}>
+        <AppContext.Provider value={{provider, address, chainId, changeAddress}}>
             <div className="App">
-                <header className="App-header">
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+                <button onClick={onConnectWallet}>Connect Wallet</button>
             </div>
         </AppContext.Provider>
     );
