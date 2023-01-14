1b5149c07b2e6f7726d918be80195d28d6a02114
alexisljn <alexis.lejeune@gmx.fr>
2023-01-14 17:17:52 +0100

    create checkSignedMessage() and generateAccessToken() in backend/AuthUtils

--- backend/src/utils/AuthUtils.ts
+++ backend/src/utils/AuthUtils.ts
@@ -1,5 +1,23 @@
+import {ethers} from "ethers";
+import {formatAddressWithChecksum} from "./Utils";
+import jwt from "jsonwebtoken";
+
+
 function generateMessageToSign(address: string) {
     return `I will prove by signing this message that I own ${address}`;
 }
 
-export {generateMessageToSign}
+function checkSignedMessage(address: string, message: string): boolean {
+    const addressFromMessage = ethers.utils.verifyMessage(
+        `I will prove by signing this message that I own ${address}`,
+        message
+    );
+
+    return formatAddressWithChecksum(addressFromMessage) === formatAddressWithChecksum(address)
+}
+
+function generateAccessToken(address: string) {
+    return jwt.sign({user: address}, process.env.APP_SECRET, {expiresIn: '100 years'}); // Never expires for purpose
+}
+
+export {generateMessageToSign, checkSignedMessage, generateAccessToken}
