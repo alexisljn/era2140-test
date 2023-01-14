acf6f63aa7e431d5a496e3dd53772a97baf38002
alexisljn <alexis.lejeune@gmx.fr>
2023-01-14 18:15:30 +0100

    make access tokens an object instead of an array

--- frontend/src/utils/AuthUtils.ts
+++ frontend/src/utils/AuthUtils.ts
@@ -8,8 +8,20 @@ async function signMessage(signer: Signer, message: string): Promise<string | nu
     }
 }
 
-function saveAccessTokenInLocalStorage(accessToken: string) {
-    window.localStorage.setItem('accessToken', accessToken);
+function saveAccessTokenInLocalStorage(address: string, accessToken: string) {
+    let accessTokens: string | null | {[key: string]: string} = window.localStorage.getItem('accessToken');
+
+    if (!accessTokens) {
+        accessTokens = {};
+    } else {
+        accessTokens = JSON.parse(accessTokens) as {[key: string]: string};
+    }
+
+    accessTokens[address] = accessToken;
+
+    window.localStorage.setItem('accessToken', JSON.stringify(accessTokens));
 }
 
-export {signMessage, saveAccessTokenInLocalStorage};
+}
+
+export {signMessage, saveAccessTokenInLocalStorage, getAccessTokenInLocalStorage};
