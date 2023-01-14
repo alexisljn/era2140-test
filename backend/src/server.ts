0f748d6825ee4d051e0de3ca8b822725c08434e4
alexisljn <alexis.lejeune@gmx.fr>
2023-01-14 17:21:42 +0100

    middleware for json body

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,24 @@
 import express, {NextFunction, Request, Response} from 'express';
 import {authRouter} from "./routers/AuthRouter";
 import {ErrorData} from "./types/CommonTypes";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const port = 3000
 
+app.use(express.json());
+
 // CORS Preflight request
-app.options('*', (req: Request, res: Response) => {
-    res.set('Access-Control-Allow-Origin', String(process.env.NODE_ENV) === 'development' ? '*' : String(process.env.FRONTEND_URL));
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+    res.set('Access-Control-Allow-Origin', String(process.env.FRONTEND_URL));
     res.set('Access-Control-Allow-Methods', 'GET, POST');
     res.set('Access-Control-Allow-Headers', 'Authorization, Content-Type');
 
+    next();
+});
+
+app.options('*', (req: Request, res: Response) => {
     res.status(200).send();
 });
 
@@ -37,7 +45,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     } catch (e) {}
 
     res.status(error.status).json({message: error.message});
-})
+});
 
 
 app.listen(port, () => {
