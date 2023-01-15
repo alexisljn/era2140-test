687ef83363ccbd36006ebf33eccc2b760689769e
alexisljn <alexis.lejeune@gmx.fr>
2023-01-15 18:42:08 +0100

    Initiate contracts with basic vars and methods

--- ethereum/contracts/QuizResult.sol
+++ ethereum/contracts/QuizResult.sol
@@ -3,10 +3,30 @@ pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
 
 contract QuizResult is Ownable, ERC721 {
+    using Counters for Counters.Counter;
 
-    constructor() {
+    Counters.Counter private _tokenIds;
 
+    string private _baseUri;
+
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        string memory baseURI_
+    ) Ownable() ERC721(name_, symbol_) {
+        _baseUri = baseURI_;
+        _tokenIds.increment();
+    }
+
+    function _baseURI() internal override view returns(string memory) {
+        return _baseUri;
+    }
+
+    function mint() external {
+        // Merkel proof
     }
 }
