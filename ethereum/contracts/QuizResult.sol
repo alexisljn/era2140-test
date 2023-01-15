265504bf3347ae266ff03e1368cd8f5a8fab9dfe
alexisljn <alexis.lejeune@gmx.fr>
2023-01-15 17:22:09 +0100

    create QuizResult contract

---
+++ ethereum/contracts/QuizResult.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract QuizResult is Ownable, ERC721 {
+
+    constructor() {
+
+    }
+}
