f430651c1e9f0bb6e898648eca330e323762955d
alexisljn <alexis.lejeune@gmx.fr>
2023-01-15 14:03:09 +0100

    Stylish Header component

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,69 +1,54 @@
-import React, {useCallback, useContext} from "react";
+import React, {useContext} from "react";
 import {AppContext} from "../../App";
-import {connectWallet} from "../../utils/ProviderUtils";
-import {fetchApi, formatAddressWithChecksum} from "../../utils/Utils";
-import {getAccessTokenInLocalStorage, saveAccessTokenInLocalStorage, signMessage} from "../../utils/AuthUtils";
+import {shortenAddress} from "../../utils/Utils";
 
 function Header() {
 
-    const {provider, address, changeAddress, hasValidToken, changeHasValidToken} = useContext(AppContext)
+    const {address, hasValidToken} = useContext(AppContext)
 
-    //TODO Deport
-    const onConnectWallet = useCallback(async () => {
-        try {
-            const address = await connectWallet(provider!);
+    if (!hasValidToken && !address) {
+        return (
+            <>
+                <div><i className="fa-solid fa-sun"></i></div>
+                <div className="not-connected-label">
+                    <i className="fa-solid fa-triangle-exclamation"></i>
+                    <span className="connection-status">Not connected</span>
+                </div>
+            </>
+        )
+    }
 
-            changeAddress(formatAddressWithChecksum(address));
-        } catch (e: any) {
-            console.error(e); // Logging for user
-        }
-    }, [provider, changeAddress]);
-
-    // TODO Deport
-    const onSignIn = useCallback(async () => {
-        try {
-            const {message} = await fetchApi(`auth/message/${address}`);
-
-            const signedMessage = await signMessage(provider!.getSigner(), message);
-
-            const {accessToken} = await fetchApi(
-                'auth',
-                'POST',
-                [{name: 'Content-Type', value: 'application/json'}],
-                {address, message: signedMessage}
-            );
-
-            saveAccessTokenInLocalStorage(address!, accessToken);
-
-            changeHasValidToken(true);
-        } catch (e: any) {
-            console.error(e);
-        }
-    }, [provider, address]);
-
-    const testToken = useCallback(async () => {
-        const token = getAccessTokenInLocalStorage(address!);
-
-        const response = await fetchApi('quiz', 'POST', [{name: 'Authorization', value: `Bearer ${token}`}])
-
-        console.log(response);
-    }, [address]);
+    if (!hasValidToken && address) {
+        return (
+            <>
+                <div><i className="fa-solid fa-sun"></i></div>
+                <div className="connected-label">
+                    <div>
+                        <i className="fa-solid fa-diagram-project"></i>
+                        <span className="connection-status">connected to TODO_NETWORK</span>
+                    </div>
+                    <div>
+                        <i className="fa-solid fa-wallet"></i>
+                        <span className="connection-status">{shortenAddress(address)}</span>
+                    </div>
+                </div>
+            </>
+        )
+    }
 
     return (
         <>
-            {hasValidToken
-                ?
-                <>
-                    <button onClick={testToken}>Test token</button>
-                    <span>Connecté</span>
-                </>
-                : address
-                    ? <button onClick={onSignIn}>Sign In</button>
-                    : <button onClick={onConnectWallet}>Connect Wallet</button>
-            }
-            {address &&
-                <span>{address}</span>
-            }
+            <div><i className="fa-solid fa-sun"></i></div>
+            <div className="connected-label">
+                <div>
+                    <i className="fa-solid fa-diagram-project"></i>
+                    <span className="connection-status">authenticated on TODO_NETWORK</span>
+                </div>
+                <div>
+                    <i className="fa-solid fa-wallet"></i>
+                    <span className="connection-status">{shortenAddress(address!)}</span>
+                </div>
+            </div>
         </>
     )
 }
