f55efd13ed941cad74374974eb6137f800454f26
alexisljn <alexis.lejeune@gmx.fr>
2023-01-16 19:41:31 +0100

    require valid merkle proof in mint()

--- ethereum/contracts/QuizResult.sol
+++ ethereum/contracts/QuizResult.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.17;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract QuizResult is Ownable, ERC721 {
     using Counters for Counters.Counter;
@@ -32,7 +32,11 @@ contract QuizResult is Ownable, ERC721 {
         merkleRoot = merkleRoot_;
     }
 
-    function mint() external {
-        // Merkel proof
+    function mint(bytes32[] calldata merkleProof_) external view returns(uint8) {
+        bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+
+        require(MerkleProof.verify(merkleProof_, merkleRoot, leaf), 'Not allowed to mint token');
+
+        return 10;
     }
 }
