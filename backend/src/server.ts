83bb410a17ea7cdcfd32c625333a4356cbea0406
alexisljn <alexis.lejeune@gmx.fr>
2023-01-16 15:38:47 +0100

    test route to update merkle root

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,7 @@ import {ErrorData} from "./types/CommonTypes";
 import dotenv from "dotenv";
 import {quizRouter} from "./routers/QuizRouter";
 import {initiateProvider} from "./utils/ProviderUtils";
+import {addAddressToMerkleTree, generateMerkleTree, isAddressInMerkleTree} from "./managers/MerkleManager";
 
 dotenv.config();
 initiateProvider();
@@ -33,6 +34,27 @@ app.use('/quiz', quizRouter);
 app.get('/', (req, res) => {
     res.send('Hello World!!')
 })
+
+app.get('/merkle/root/:address(\\w+)', (req, res, next) => {
+    try {
+        if (!isAddressInMerkleTree("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")) {
+
+            addAddressToMerkleTree("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+
+            const merkleTree = generateMerkleTree();
+
+            console.log(merkleTree.getHexRoot());
+
+            res.status(201).end();
+        }
+        console.log('no update');
+        res.status(200).end();
+    } catch (e) {
+        console.log(e)
+        next(e);
+    }
+
+});
 
 // 404
 app.use(({next}) => {
