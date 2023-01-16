7867c90ebff30a2d2245826dfc49693e8adfe0e0
alexisljn <alexis.lejeune@gmx.fr>
2023-01-16 21:57:48 +0100

    create type Question

--- backend/src/types/CommonTypes.ts
+++ backend/src/types/CommonTypes.ts
@@ -2,3 +2,8 @@ export interface ErrorData {
     status: number;
     message: string;
 }
+
+export interface Question {
+    content: string;
+    answers: Array<{label: string, isRight: boolean}>
+}
