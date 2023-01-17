6d08737f1f429f7d75ca692d2b9b74b102ddad6f
alexisljn <alexis.lejeune@gmx.fr>
2023-01-17 20:38:58 +0100

    create methods to handle localStorage

--- frontend/src/utils/Utils.ts
+++ frontend/src/utils/Utils.ts
@@ -45,4 +45,17 @@ function shortenAddress(address: string) {
     return `${address.slice(0, 4)}...${address.slice(-4)}`;
 }
 
-export {formatAddressWithChecksum, fetchApi, shortenAddress}
+function setItemToLocalStorage(key: string, value: any) {
+    window.localStorage.setItem(key, JSON.stringify(value));
+}
+function getItemFromLocalStorage(key: string): any {
+    const item = window.localStorage.getItem(key);
+
+    return item ? JSON.parse(item) : null;
+}
+
+function deleteItemFromLocalStorage(key: string): any {
+    window.localStorage.removeItem(key);
+}
+
+export {formatAddressWithChecksum, fetchApi, shortenAddress, setItemToLocalStorage, getItemFromLocalStorage, deleteItemFromLocalStorage}
